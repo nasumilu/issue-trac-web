bb9c03a66fba01c8f81772476399570ef3499f0e
Michael Lucas <nasumilu@gmail.com>
2023-04-16 02:24:05 -0400

    added prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: true,
+  map: {
+    center: [-11209356.066861978, 4764718.121160403],
+    zoom: 5,
+    projection: 'EPSG:3857'
+  },
+  geocoder: 'nasumilu',
   services: {
-    geocode: 'https://nasumilu.io/services'
+    geocode: 'https://nasumilu.io/services',
+    account: 'https://nasumilu.io/auth/token?info=json'
   }
 };
